5f677672bd81351f31f8fd45353fc1ed1c5405b4
Amit Kundu <kundu.amit517@gmail.com>
2021-07-23 23:28:16 +0600

    Welcome Screen

--- App.js
+++ App.js
@@ -1,67 +1,11 @@
 import React from "react";
 import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   const handelPress = () => console.log("Text Clicked");
 
-  return (
-    <View
-      style={{
-        backgroundColor: "white",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center", // main
-        alignItems: "center",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          top: 20,
-          left: 20,
-          position: "absolute",
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-      {/* <View
-        style={{
-          backgroundColor: "gray",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "greenyellow",
-          width: 100,
-          height: 100,
-        }}
-      /> */}
-    </View>
-  );
+  return <WelcomeScreen />;
 }
 
 const backGroundStyle = { backgroundColor: "white" };
-
-/* const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-}); */
