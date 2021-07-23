e14c171a2bde92656bb1d52f5eeb2f2c0a960656
Amit Kundu <kundu.amit517@gmail.com>
2021-07-23 22:14:00 +0600

    flex

--- App.js
+++ App.js
@@ -1,45 +1,47 @@
 import React from "react";
-import {
-  Dimensions,
-  Alert,
-  Button,
-  Platform,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  View,
-} from "react-native";
-import {
-  useDeviceOrientation,
-  useDimensions,
-} from "@react-native-community/hooks";
+import { View } from "react-native";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-  console.log(useDeviceOrientation());
-  console.log(useDimensions());
-
   const handelPress = () => console.log("Text Clicked");
 
   return (
-    <SafeAreaView style={[styles.container, backGroundStyle]}>
+    <View
+      style={{
+        backgroundColor: "dodgerblue",
+        flex: 1,
+        flexDirection: "row",
+      }}
+    >
       <View
         style={{
-          backgroundColor: "blue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
+          backgroundColor: "red",
+          width: 100,
+          height: 100,
         }}
-      ></View>
-    </SafeAreaView>
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
+    </View>
   );
 }
 
 const backGroundStyle = { backgroundColor: "white" };
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
-});
+}); */
