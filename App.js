0865ee2ba03926afc84afac681748caf8aef1cab
Amit Kundu <kundu.amit517@gmail.com>
2021-07-27 20:27:29 +0600

    bordars

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import React from "react";
 import { View } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   const handelPress = () => console.log("Text Clicked");
 
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          borderWidth: 10,
+          borderColor: "royalblue",
+          borderRadius: 50,
+        }}
+      ></View>
+    </View>
+  );
 }
 
 const backGroundStyle = { backgroundColor: "white" };
