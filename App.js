d13eabc4c1276a7eba59bec495d8b6e1b32e02b1
Amit Kundu <kundu.amit517@gmail.com>
2021-07-27 20:31:49 +0600

    shadow

--- App.js
+++ App.js
@@ -19,9 +19,13 @@ export default function App() {
           backgroundColor: "dodgerblue",
           width: 100,
           height: 100,
-          borderWidth: 10,
-          borderColor: "royalblue",
-          borderRadius: 50,
+          //for ios
+          shadowColor: "gray",
+          shadowOffset: { width: 10, height: 10 },
+          shadowOpacity: 1,
+          shadowRadius: 10,
+          // for android
+          elevation: 20,
         }}
       ></View>
     </View>
