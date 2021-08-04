736813c2e176cc65c416c0446e898348a5ce23df
Amit Kundu <kundu.amit517@gmail.com>
2021-08-04 17:04:54 +0600

    icon

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-
-import AppText from "./app/components/AppText";
+import {MaterialCommunityIcons} from '@expo/vector-icons'
 export default function App() {
-  const handelPress = () => {
-    console.log("Text Clicked");
-  };
-
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <AppText>
-        I love react native
-      </AppText>
+    <View>
+      <MaterialCommunityIcons name = 'email' size = {60} color = 'dodgerblue'/>
     </View>
   );
 }
