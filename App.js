55933d9f9f5a42e183d3352a74b6a1a75bf868ff
Amit Kundu <kundu.amit517@gmail.com>
2021-08-04 17:29:59 +0600

    Platform in same component

--- App.js
+++ App.js
@@ -2,11 +2,21 @@ import React from "react";
 import { View, Text } from "react-native";
 
 import {MaterialCommunityIcons} from '@expo/vector-icons'
+import AppText from "./app/components/AppText";
+
 export default function App() {
   return (
-    <View>
-      <MaterialCommunityIcons name = 'email' size = {60} color = 'dodgerblue'/>
-    </View>
+    <View
+    style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <AppText>
+      I love react native
+    </AppText>
+  </View>
   );
 }
 
