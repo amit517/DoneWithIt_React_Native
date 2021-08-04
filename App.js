21a416b44d4b94066c6f9d603c7ce3d615ff94cc
Amit Kundu <kundu.amit517@gmail.com>
2021-08-04 16:59:45 +0600

    encapsulating style [make custom component]

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
-import colors from "./app/config/colors";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
 
+
+import AppText from "./app/components/AppText";
 export default function App() {
-  const handelPress = () => console.log("Text Clicked");
+  const handelPress = () => {
+    console.log("Text Clicked");
+  };
 
   return (
     <View
@@ -15,22 +16,9 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <Text
-        style={{
-          fontSize: 30,
-          // font's are plat from dependent. So need to check the fonts before using
-          fontFamily: "Roboto",
-          fontStyle: "italic",
-          fontWeight: "600", // must be string
-          color: "red",
-          textTransform: "capitalize",
-          textDecorationLine: "line-through",
-          textAlign: "center",
-          lineHeight: 30,
-        }}
-      >
-        I love react native, this a is very long text... ha ha haa.. have fun
-      </Text>
+      <AppText>
+        I love react native
+      </AppText>
     </View>
   );
 }
